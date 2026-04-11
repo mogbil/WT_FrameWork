baa5e2a8621b7600080a61ea450710362f64bbc5
Mogbil Sourketti <23183995+mogbil@users.noreply.github.com>
2026-04-11 14:25:15 +0300

    Update

--- pub_wt/js/wt.js
+++ pub_wt/js/wt.js
@@ -1,3 +1,12 @@
+/***********************************************************************
+ # *          @Project    : WT Store
+ # *          @version    : 1.1
+ # *          @author     : Mogbil Sourketti info[@]wondtech.com
+ # *          @copyright  : 2020 WondTech for Integrated Digital Solutions
+ # *          @link       : http://www.wondtech.com
+ # *          @package    : WT FrameWork (1.1)
+ # ************************************************************************/
+
 $(function(){
 
 });
